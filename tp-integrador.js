377b7bf49237d5ff765fbe5ba4562ec2ea9fbc6e
MBerenice5 <michelleberenicedelavega@gmail.com>
2025-04-09 19:00:42 -0300

    Punto dos completo.

--- tp-integrador.js
+++ tp-integrador.js
@@ -31,3 +31,83 @@ let usuarios = [
     {id: 567, nombre: "Lorenzo Salome", email: "lolito@gmail.com", librosPrestados: [10] }
 ]
 console.log(usuarios)
+
+//PUNTO DOS: Funciones de Gestión de Libros
+
+//A) Implementar una función agregarLibro(id, titulo, autor, anio, genero) que agregue un nuevo libro al array libros.
+
+function agregarLibro(id, titulo, autor, anio, genero) {
+    const nuevoLibro = {
+        id: id,
+        titulo: titulo,
+        autor: autor,
+        anio: anio,
+        genero: genero,
+        disponible: true
+    };
+    libros.push(nuevoLibro);
+    console.log(`El libro ${titulo} ha sido agregado al catálogo.`);
+}
+
+// Ejemplo de uso
+agregarLibro(11, "El principito", "Antoine de Saint-Exupéry", 1943, "Ficción");
+console.log(libros);
+
+//B) Crear una función buscarLibro(criterio, valor) que permita buscar libros por título, autor o género
+// utilizando el algoritmo de búsqueda lineal (recorre los elementos de una lista hasta encontrar lo que la condición).
+
+function buscarLibro(criterio, valor) {
+    const resultados = [];
+
+    for (let i = 0; i < libros.length; i++) {
+        const libro = libros[i];
+
+        if (
+            libro[criterio] &&
+            libro[criterio].toLowerCase().includes(valor.toLowerCase())
+        ) {
+            resultados.push(libro);
+        }
+    }
+
+    return resultados;
+}
+
+//C)Desarrollar una función ordenarLibros(criterio) que ordene los libros por título o año
+// utilizando el algoritmo de ordenamiento burbuja (bubble sort) (for anidado para comparar y ordenar (Ver ejercicio 7 de la clase 10 - intercambio de valores)
+// y luego muestre los libros ordenados en la consola.
+
+function ordenarLibros(criterio) {
+    for (let i = 0; i < libros.length - 1; i++) {
+        for (let j = 0; j < libros.length - 1 - i; j++) {
+            let actual = libros[j];
+            let siguiente = libros[j + 1];
+
+            if (
+                (criterio === "titulo" && actual.titulo.toLowerCase() > siguiente.titulo.toLowerCase()) ||
+                (criterio === "anio" && actual.anio > siguiente.anio)
+            ) {
+                let temp = libros[j];
+                libros[j] = libros[j + 1];
+                libros[j + 1] = temp;
+            }
+        }
+    }
+
+    console.log("Libros ordenados por", criterio);
+    console.log(libros);
+}
+
+//D) Desarrollar una función borrarLibro(id) que elimine el libro que se le pase por parámetro
+
+function borrarLibro(id) {
+    for (let i = 0; i < libros.length; i++) {
+        if (libros[i].id === id) {
+            libros.splice(i, 1);
+            console.log(`Libro con ID ${id} eliminado del catálogo.`);
+            return;
+        }
+    }
+
+    console.log(`No se encontró ningún libro con el ID ${id}.`);
+}
